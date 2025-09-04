a845c58483095ad8a425687289544365b32e814b
Simon Barbé <SimonB@MacBook-pro-Simon.local>
2025-09-04 12:06:51 +0200

    main + asso

--- frontend/src/utils/pong/pong_local.ts
+++ frontend/src/utils/pong/pong_local.ts
@@ -3,7 +3,7 @@ const pong_local: (container: HTMLElement) => void = (container) => {
 	<div id="local-options" class="flex flex-col items-center justify-center mt-8 space-y-4 space-x-4">
 	  <button id="nav-game-configplay" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">🎮 Match Libre ⚡</button>
 	  <button id="nav-game-configtourn" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">🎮 Tournois</button>
-	</div>
+	</div> 
 	`;
 };
 
